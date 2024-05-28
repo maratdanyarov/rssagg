981fa2921e9af089473dd5eba93548ce7dddc0c3
MaratD87 <maratdanyarov87@gmail.com>
2024-05-28 15:10:24 +0100

    boilerplate for http server is complete

--- main.go
+++ main.go
@@ -28,6 +28,11 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
